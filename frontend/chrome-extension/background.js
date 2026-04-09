56bfa8b3e922c41cc15d5a1a2598d0ac51c6528f
hilary <165844061+ribbonpuff@users.noreply.github.com>
2026-04-09 17:10:17 -0400

    login endpoint

--- frontend/chrome-extension/background.js
+++ frontend/chrome-extension/background.js
@@ -1,20 +1,91 @@
-console.log("Background script loaded");
-chrome.history.onVisited.addListener(visit);
-console.log("History listener added");
+var lastVisitedURL = "";
+let debounceTimeout;
 
-function visit(result) {
-    setTimeout(() => {
-        console.log("Visited " + result.title + 
-        " at " + timeFormat(result?.lastVisitTime) + 
-        "\nURL:" + result.url);
-    }, 500);
-};
+console.log("Background script loaded");
+chrome.history.onVisited.addListener(function(historyItem) {
+    visit(historyItem);
+});
+console.log("History listener added");
 
 function timeFormat(time) {
     var d = new Date(time);
     return d.toLocaleString();
 }
-// chrome.history.onVisited.addListener((result) => {
-//     chrome.runtime.sendMessage({ type: "historyUpdate", data: result });
-// });
+
+function isLoggedIn() {
+    chrome.storage.local.get("loggedIn", (result) => {
+        const loggedIn = result.loggedIn || false;
+        console.log("Checked login status: ", loggedIn);
+    });
+    return loggedIn;
+}
+
+function getToken() {
+    chrome.storage.local.get("token", (result) => {
+        const token = result.token || "";
+        console.log("Retrieved token: Z", token.length>0 ? "length " + token.length : "none");
+    });
+    return token;
+}
+
+function getDeviceID() {
+    chrome.storage.local.get("deviceID", (result) => {
+        const deviceID = result.deviceID || "";
+        console.log("Retrieved device ID: ", deviceID);
+    });
+    return deviceID;
+}
+
+
+var loggedIn = isLoggedIn();
+
+var token = chrome.storage.local.get("token", (result) => {
+    token = result.token || "";
+    console.log("Initial token:", token.substring(0, 3) + "...");
+});
+
+// Record user history activity
+function visit(result) {
+    //console.log("Last Visited URL:", result.url);
+    if (result.url === (lastVisitedURL)) {
+        //console.log("URL already visited, skipping");
+        return;
+    }
+    lastVisitedURL = result.url;
+    const title = result.title ? result.title : "";
+    if (title.length == 0){
+        chrome.history.addUrl({url: result.url}, () => {console.log("Trying again for title")});
+        return;
+    }
+
+    // Reset the debouncing timeout to handle the most recent visit
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => {
+        console.log("Visited " + title + " at " + timeFormat(result?.lastVisitTime) + 
+        "\nURL:" + result.url);
+    },100); // 0.1 second delay to clean up (you can adjust this)
+    
+
+    if (isLoggedIn()) {
+        // Add search to database
+        fetch("https://senior-project-production-4c90.up.railway.app/searches", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "x-api-key": getToken()
+            },
+            body: JSON.stringify({
+                deviceid: "d01",
+                query_text: "test search",
+                url: window.location.href
+            })
+        })
+        .then(async (res) => {
+            console.log("status:", res.status);
+            const body = await res.text();
+            console.log("body:", body);
+        })
+        .catch(err => console.error("fetch error:", err));
+    }
+}
 
--- frontend/chrome-extension/popup/popup.js
+++ frontend/chrome-extension/popup/popup.js
@@ -1,38 +1,124 @@
 chrome.extension.inIncognitoContext = true;
 
-var loggedIn = false;
+var loggedIn = getLoggedIn();
+document.getElementById("loginContainer").hidden = loggedIn;
+
+
+// Form submission handlers for login and device addition
 document.getElementById("loginForm").addEventListener("submit", logIn);
+document.getElementById("deviceForm").addEventListener("submit", addDevice);
+
+// Log in user
 function logIn(event) {
     event.preventDefault();
-
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const message = document.getElementById("message");
+    message.style.color = "green";
+    message.textContent = "Attempting login with email: " + email;
 
-   
-    if (email === "parent@example.com" && password === "123456") {
-        message.style.color = "green";
-        message.textContent = "Redirecting...";
-        loggedIn = true;
-        document.getElementById("loginContainer").hidden = loggedIn;
-        document.getElementById("deviceContainer").hidden = !loggedIn;
-    } 
-    else {
-        message.style.color = "red";
-        message.textContent = "Invalid email or password.";
-    }
+    fetch("https://senior-project-production-4c90.up.railway.app/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "email": email,
+            "password": password
+        })
+    })
+    .then(async (res) => {
+        var loginBody = await res.text();
+        if (loginBody.includes("bearer")) {
+            var token = JSON.parse(loginBody).access_token;
+            message.style.color = "green";
+            message.textContent = "Login Successful!";
+            loggedIn = true;
+            
+            chrome.storage.local.set({ loggedIn: true, token: token, email: email, userID: await getUserID(email, token) });
+            document.getElementById("loginContainer").hidden = loggedIn;
+            document.getElementById("deviceContainer").hidden = !loggedIn;
+        } 
+        else {
+            message.style.color = "red";
+            message.textContent = "Invalid email or password.";
+        }
+    })
+    .catch(err => console.error("fetch error:", err));
+
+    
 };
 
-document.getElementById("deviceForm").addEventListener("submit", addDevice);
+// Get userid from email
+async function getUserID(email, token) {
+    fetch("https://senior-project-production-4c90.up.railway.app/userid", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "email": email,
+            "x-api-key": token
+        })
+    })
+    .then(async (res) => {
+        const userID = await res.text();
+        return userID;
+    })
+    .catch(err => console.error("fetch error:", err));
+}
+
+// Add device for monitoring
 function addDevice(event) {
     event.preventDefault();
     const deviceName = document.getElementById("deviceName").value;
     const message = document.getElementById("deviceMessage");
-    document.getElementById("deviceContainer").hidden = true;
-    document.getElementById("deviceTitle").textContent = deviceName;
-    document.getElementById("successContainer").hidden = false;
-    // // open history tab when extension icon clicked
-    // chrome.tabs.create({
-    //     url: chrome.runtime.getURL("history/history.html")
-    // });
+    const userID = getUserID(); // This should be retrieved from the login response in a real implementation
+
+    fetch("https://senior-project-production-4c90.up.railway.app/devices", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "userid": userID,
+            "device_name": deviceName
+        })
+    })
+    .then(async (res) => {
+        console.log("status:", res.status);
+        const deviceBody = await res.text();
+        console.log("body:", deviceBody);
+        if (!deviceBody.stringify.includes("detail")) {
+            document.getElementById("deviceContainer").hidden = true;
+            document.getElementById("deviceTitle").textContent = deviceName;
+            document.getElementById("successContainer").hidden = false;
+        }
+    })
+    .catch(err => console.error("fetch error:", err));
+
+}
+
+function getLoggedIn() {
+    var loggedIn = false;
+    chrome.storage.local.get("loggedIn", (result) => {
+        loggedIn = result.loggedIn || false;
+    });
+    return loggedIn;
+}
+
+function getUserID() {
+    var userID = "";
+    chrome.storage.local.get("userID", (result) => {
+        userID = result.userID || "";
+    });
+    return userID;
+}
+
+function getToken() {
+    var token = "";
+    chrome.storage.local.get("token", (result) => {
+        token = result.token || "";
+    });
+    return token;
 }
